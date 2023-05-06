ab3b5345822c601547ff6ce3d4c2c7ed8f7090a3
Jihyun-Choi <034179@naver.com>
2023-05-07 02:31:10 +0900

    feat: swagger 설정

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,41 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path, include
 
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+openapi_info = openapi.Info(
+    title="ASKu API",
+    default_version="v1",
+    description="API description",
+    terms_of_service="https://www.google.com/policies/terms/",
+    contact=openapi.Contact(email="034179@gmail.com"),
+    license=openapi.License(name="MIT LICENSE"),
+)
+
+schema_view = get_schema_view(
+    openapi_info,
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    re_path(
+        r"^swagger(?P<format>\.json|\.yaml)$",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    re_path(
+        r"^swagger/$",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    re_path(
+        r"^redoc/$",
+        schema_view.with_ui("redoc", cache_timeout=0),
+        name="schema-redoc",
+    ),
 ]
