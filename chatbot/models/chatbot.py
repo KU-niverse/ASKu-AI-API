8ea869b134e3be002f8c6de8ac9122cb9e06dcc1
Jihyun-Choi <034179@naver.com>
2023-06-24 15:42:26 +0900

    refactor: Chatbot model

--- chatbot/models/chatbot.py
+++ chatbot/models/chatbot.py
@@ -5,27 +5,32 @@ from django.utils.translation import gettext_lazy as _
 class Chatbot(models.Model):
     """Model definition for Chatbot."""
 
-    user = models.CharField(
+    session_id = models.CharField(
         max_length=50,
         null=True,
         blank=True,
     )  # 사용자
-    text = models.TextField(
+    content = models.TextField(
         null=False,
         blank=False,
         # TODO: required 유효성 검사 추가
     )  # 내용
-    data = models.JSONField(
+    type = models.BooleanField(
+        null=False,
+        blank=False,
+        default=False,
+    )  # 질문:0, 답변:1
+    reference = models.TextField(
         null=True,
         blank=True,
-    )  # gpt3 json
+    )  # 학습 내용
     created_at = models.DateTimeField(
         verbose_name=_("created at"),
         auto_now_add=True,
     )  # Chatbot 레코드가 생성된 일자
 
-    # def __str__(self):
-    #     return self.user
+    def __str__(self) -> str:
+        return f"[{self.id}] {self.name}"
 
     class Meta:
         verbose_name = "Chatbot"
