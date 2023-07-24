aac431fee684ec36a9abf045f46f5c824496acc5
Jihyun-Choi <034179@naver.com>
2023-07-24 18:09:36 +0900

    feat: feedback API 구현

---
+++ chatbot/views/feedback_create_api_view.py
@@ -0,0 +1,31 @@
+from rest_framework.generics import CreateAPIView
+from rest_framework import generics, status
+from rest_framework.response import Response
+
+from chatbot.serializers import FeedbackSerializer
+from chatbot.utils.db_query import is_feedback, is_not_qna_id
+
+
+class FeedbackCreateAPIView(CreateAPIView):
+    """FeedbackCreateView
+    POST: api/chatbot/feedback/
+    """
+    serializer_class = FeedbackSerializer
+
+    def post(self, request, *args, **kwargs):
+
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        qna_id = request.data['qna_id']
+        feedback = request.data['feedback']
+
+        if is_not_qna_id(qna_id):
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        if is_feedback(qna_id):
+            return Response(status=status.HTTP_403_FORBIDDEN)
+
+        serializer.save(
+            qna_id=qna_id,
+            feedback=feedback,
+        )
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
