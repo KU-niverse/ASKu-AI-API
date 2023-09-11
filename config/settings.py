1c20c7b33b9ae0f5065d2897f920c2557f54184a
Jihyun-Choi <034179@naver.com>
2023-09-11 18:13:59 +0900

    feat: pip install gunicorn

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 import environ
 from pathlib import Path
 from dotenv import load_dotenv
@@ -150,3 +151,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# 정적 파일 관련 설정
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# 미디어 파일 관련 설정
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
