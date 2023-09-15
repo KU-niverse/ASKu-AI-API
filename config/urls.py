1dd2e17821e0f4336b9ae6595b6d175e93d4a674
Jihyun-Choi <034179@naver.com>
2023-09-15 20:33:31 +0900

    fix: static urls 수정

--- config/urls.py
+++ config/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, re_path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
@@ -59,3 +61,4 @@ urlpatterns = [
         name="schema-redoc",
     ),
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
