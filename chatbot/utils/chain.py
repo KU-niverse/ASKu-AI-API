1481f8d459b56c0836b55b262e5d72f8c3117b30
Jihyun-Choi <034179@naver.com>
2024-02-26 17:09:48 +0900

    refactor: ChatbotConfig 수정
    - ready_chain() 함수화
    : 서버 시작 시 실행되어야하는 RAG LLM 코드
    - path 환경변수 처리

---
+++ chatbot/utils/chain.py
@@ -0,0 +1,60 @@
+import os
+import pickle
+
+from django.conf import settings
+from langchain_openai.embeddings import OpenAIEmbeddings
+from langchain.vectorstores.redis import Redis
+from langchain_openai import ChatOpenAI
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents.stuff import create_stuff_documents_chain
+from langchain.retrievers import MergerRetriever, MultiVectorRetriever
+from langchain.prompts import ChatPromptTemplate, SystemMessagePromptTemplate
+from langchain.storage.in_memory import InMemoryStore
+
+
+def ready_chain():
+    # Construct MergerRetriever
+    # MergerRetriever
+    # ├ MultiVectorRetriever
+    # └ VectorstoreRetriever
+    pickle_path = os.getenv("PICKLE_PATH")
+    with open(pickle_path, "rb") as f:
+        docstore: InMemoryStore = pickle.load(f)
+
+    Wiki_Redis = Redis.from_existing_index(
+        redis_url=os.getenv("REDIS_URL"),
+        index_name=os.getenv("WIKI_INDEX"),
+        embedding=OpenAIEmbeddings(),
+        schema={
+            "text": [{'name': 'doc_id'}]  # MUST be SAME with source_id_key
+        })
+
+    Wiki_retriever = MultiVectorRetriever(
+        vectorstore=Wiki_Redis,
+        docstore=docstore,
+        id_key="doc_id",
+    )
+    Rule_retriever = Redis(
+        redis_url=os.getenv("REDIS_URL"),
+        index_name=os.getenv("RULE_INDEX"),
+        embedding=OpenAIEmbeddings()).as_retriever()
+
+    contextRetriever = MergerRetriever(retrievers=[Wiki_retriever, Rule_retriever])
+
+    # Construct create_stuff_document_chain(name: llm_chain)
+    # create_stuff_document_chain
+    # ├ ChatPromptTemplate
+    # ├─ SystemPromptTemplate
+    # └ ChatOpenAI
+    llm = ChatOpenAI(model="gpt-3.5-turbo")
+    with open("systemprompt.txt", "r", encoding="utf-8") as f:
+        template = f.read()
+    Systemprompt = SystemMessagePromptTemplate.from_template(template=template)
+    chat_prompt = ChatPromptTemplate.from_messages([Systemprompt])
+    llm_chain = create_stuff_documents_chain(llm=llm, prompt=chat_prompt)
+
+    # Construct create_retrieval_chain(name: QueryChain)
+    # ├ MergerRetriever(name: contextRetriever)
+    # └ create_stuff_document_chain(name: llm_chain)
+    QueryChain = create_retrieval_chain(retriever=contextRetriever, combine_docs_chain=llm_chain)
+    setattr(settings, "QueryChain", QueryChain)
