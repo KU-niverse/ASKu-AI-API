6f5931dad5dc73a4c07c3f5f897296aadfc1562f
HyeonJin-Yang <yanghj1490@korea.ac.kr>
2024-05-15 13:02:13 +0900

    Style: 개행 추가 및 타입힌트 위치 변경

--- evaluation/utils/batch.py
+++ evaluation/utils/batch.py
@@ -5,6 +5,7 @@ from langchain_core.runnables import Runnable
 from langchain.schema import Document, BaseRetriever
 from tqdm import tqdm
 
+
 async def retrieval_batch(
         retriever: BaseRetriever, 
         question: List[str], 
@@ -12,18 +13,18 @@ async def retrieval_batch(
     ) -> List[List[Document]]:
     results: List[List[Document]] = []
     batch_list: List[Coroutine] = []
-    batch_results: List[List[Document]]
+
     for item in tqdm(question):        
         batch_list.append(retriever.ainvoke(item))
 
         if len(batch_list) < concurrency_level: continue
         
-        batch_results = await asyncio.gather(*batch_list, return_exceptions=True)
+        batch_results: List[List[Document]] = await asyncio.gather(*batch_list, return_exceptions=True)
         results.extend(batch_results)
         batch_list = []
     
     if batch_list:
-        batch_results = await asyncio.gather(*batch_list, return_exceptions=True)
+        batch_results: List[List[Document]] = await asyncio.gather(*batch_list, return_exceptions=True)
         results.extend(batch_results)
     
     return results
@@ -37,18 +38,18 @@ async def generation_batch(
     ) -> List[str]:
     results: List[str] = []
     batch_list: List[Coroutine] = []
-    batch_results: List[str]
+
     for q, c in tqdm(list(zip(question, contexts))):        
         batch_list.append(generator.ainvoke({"input": q, "context": c}))
     
         if len(batch_list) < concurrency_level: continue
 
-        batch_results = await asyncio.gather(*batch_list, return_exceptions=True)
+        batch_results: List[str] = await asyncio.gather(*batch_list, return_exceptions=True)
         results.extend(batch_results)
         batch_list = []
     
     if batch_list:
-        batch_results = await asyncio.gather(*batch_list, return_exceptions=True)
+        batch_results: List[str] = await asyncio.gather(*batch_list, return_exceptions=True)
         results.extend(batch_results)
     
     return results
