7fe390d274a804d5678f925b006406180a964912
HyeonJin-Yang <yanghj1490@korea.ac.kr>
2024-07-29 22:27:37 +0900

    feat: Chat Prompt 로드 기능 추가

--- evaluation/tools/generators/prompt.py
+++ evaluation/tools/generators/prompt.py
@@ -6,18 +6,18 @@ from langfuse import Langfuse
 
 
 def load_prompt(
-        prompt_name: str, 
+        prompt_name: str,
         prompt_version: Optional[int] = None,
     ) -> ChatPromptTemplate:
     """Loads a LangFuse prompt and returns it as ChatPromptTemplate type.
-    
+
     Args:
         prompt_name (str): The prompt name on LangFuse dashboard.
         prompt_version (int, optional): The version of prompt.
-    
+
     Returns:
         ChatPromptTemplate: ChatPromptTemplate constructed using LangFuse prompt.
-    
+
     Example:
         ```python
         load_prompt(prompt_name="RAG", prompt_version=1)
@@ -25,7 +25,16 @@ def load_prompt(
     """
 
     langfuse = Langfuse(); langfuse.auth_check()
-    template = langfuse.get_prompt(name=prompt_name, version=prompt_version).prompt
-    system_message = SystemMessagePromptTemplate.from_template(template=template)
-    
-    return ChatPromptTemplate.from_messages([system_message])
+    prompt = langfuse.get_prompt(name=prompt_name, version=prompt_version).prompt
+
+    # Text Prompt
+    if type(prompt) == str:
+        messages = [SystemMessagePromptTemplate.from_template(template=prompt)]
+
+    # Chat Prompt
+    if type(prompt) == list:
+        messages = []
+        for message in prompt:
+            messages.append((message["role"], message["content"]))
+
+    return ChatPromptTemplate.from_messages(messages)
